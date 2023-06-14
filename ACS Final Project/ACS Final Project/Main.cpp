5ea78bd3ab53c1364c3fb5c62f035d2126e3e080
unknown <1012082742@qq.com>
2023-06-14 20:42:26 +0100

    Update more notes

--- ACS Final Project/ACS Final Project/Main.cpp
+++ ACS Final Project/ACS Final Project/Main.cpp
@@ -9,11 +9,11 @@ using namespace DirectX;
 HWND g_hwnd;
 HINSTANCE g_hInstance;
 
-
+//定义顶点结构体
 struct GRS_VERTEX
 {
-    XMFLOAT3 position;
-    XMFLOAT4 color;
+    XMFLOAT3 position;//等价于float[3]
+    XMFLOAT4 color;//等价于float[4]
 };
 
 //过程函数
@@ -33,29 +33,43 @@ LRESULT CALLBACK WinProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 
 int APIENTRY wWinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, _In_ LPWSTR lpCmdLine, _In_ int nCmdShow)
 {
-
+    //每帧缓冲区的数量，双缓冲为2，三缓冲为3，后面的u是后缀，将数字解释为无符号整型
     const UINT nFrameBackBufCount = 3u;
-    const float	faClearColor[] = { 0.0f, 0.0f, 0.0f, 1.0f };
-    int iWidth = 1024;
-    int iHeight = 768;
+    //每帧缓冲区索引，上面可见每帧缓冲区数量为3，则这里的索引即为0，1，2
     UINT nFrameIndex = 0;
-    UINT nFrame = 0;
 
-    UINT nDXGIFactoryFlags = 0U;
-    UINT nRTVDescriptorSize = 0U;
+    //用于clear的背景颜色
+    const float	faClearColor[] = { 0.0f, 0.0f, 0.0f, 1.0f };
+    
+    int iWidth = 1024;//窗口宽度
+    int iHeight = 768;//窗口高度
+    
+    //UINT nFrame = 0;
+    
+    //0u指的是：将数字0解释为无符号整数类型，u为后缀
+    UINT nDXGIFactoryFlags = 0u;//用于创建DX工厂的flag，因为创建工厂需要提供一个flag
+    
+    UINT nRTVDescriptorSize = 0u;//堆描述符的大小
 
-    float fAspectRatio = 3.0f;
+    float fAspectRatio = 3.0f;//用于控制绘制三角形大小的比例值
 
-    MSG msg;
+    MSG msg;//窗口的消息
 
+    //一个用于提供给绘制命令队列的顶点buffer的view，描述了顶点buffer的信息
     D3D12_VERTEX_BUFFER_VIEW stVertexBufferView = {};
-
+    
+    //围栏的标记值，0为数字，ui64为后缀，表示将0指定数值为无符号 64 位整数类型
     UINT64 n64FenceValue = 0ui64;
+
+    //用于存放创建的event对象的指针
     HANDLE hFenceEvent = nullptr;
 
+    //用于提供给绘制命令队列的DX视口，此处直接赋值为：左上角位置（0，0），窗口宽和高
     CD3DX12_VIEWPORT stViewPort(0.0f, 0.0f, static_cast<float>(iWidth), static_cast<float>(iHeight));
+    
+    //用于提供给绘制命令队列的裁切矩形，用于限制可见区域，此处大小和位置与视口一致
     CD3DX12_RECT  stScissorRect(0, 0, static_cast<LONG>(iWidth), static_cast<LONG>(iHeight));
-
+    
     ComPtr<IDXGIFactory5>                pIDXGIFactory5;
     ComPtr<IDXGIAdapter1>                pIAdapter;
     ComPtr<ID3D12Device4>                pID3DDevice;
@@ -124,23 +138,27 @@ int APIENTRY wWinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance
 
     //创建factory，以便为后续部分创建适配器，设备等
     CreateDXGIFactory2(nDXGIFactoryFlags, IID_PPV_ARGS(&pIDXGIFactory5));
+    
     //关闭ALT+ENTER键切换全屏的功能，因为没有实现OnSize处理，所以先关闭
     pIDXGIFactory5->MakeWindowAssociation(g_hwnd, DXGI_MWA_NO_ALT_ENTER);
 
 
     //枚举并选择一个合适的显示适配器（显卡）来创建D3D设备接口
-    for (UINT adapterIndex = 0
-        ; DXGI_ERROR_NOT_FOUND != pIDXGIFactory5->EnumAdapters1(adapterIndex, &pIAdapter)
-        ; ++adapterIndex)
+    for (UINT adapterIndex = 0; //适配器索引
+        DXGI_ERROR_NOT_FOUND != pIDXGIFactory5->EnumAdapters1(adapterIndex, &pIAdapter); //枚举适配器
+        ++adapterIndex)
     {
-        DXGI_ADAPTER_DESC1 desc = {};
-        pIAdapter->GetDesc1(&desc);
-        if (desc.Flags & DXGI_ADAPTER_FLAG_SOFTWARE)//可在此处打断点，查看选择了哪张显卡作为适配器创建设备
-        {//软件虚拟适配器，跳过
+        DXGI_ADAPTER_DESC1 desc = {};//声明存放适配器结构体的变量
+        pIAdapter->GetDesc1(&desc);//获取适配器结构体信息
+
+        //可在此处打断点，查看选择了哪张显卡作为适配器创建设备
+        if (desc.Flags & DXGI_ADAPTER_FLAG_SOFTWARE)//如果是软件虚拟适配器，跳过
+        {
             continue;
         }
-        //检查适配器对D3D支持的兼容级别，这里直接要求支持12.1的能力，注意返回接口的那个参数被置为了nullptr，这样
-        //就不会实际创建一个设备了，也不用我们啰嗦的再调用release来释放接口。这也是一个重要的技巧，请记住！
+        
+        //检查适配器对D3D支持的兼容级别，这里直接要求支持12.1的能力，注意返回接口的那个参数被置为了nullptr，
+        // 这样就不会实际创建一个设备了，也不用再调用release来释放接口。这是一个重要的技巧
         if (SUCCEEDED(D3D12CreateDevice(pIAdapter.Get(), D3D_FEATURE_LEVEL_12_1, _uuidof(ID3D12Device), nullptr)))
         {
             break;
@@ -150,14 +168,15 @@ int APIENTRY wWinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance
     D3D12CreateDevice(pIAdapter.Get(), D3D_FEATURE_LEVEL_12_1, IID_PPV_ARGS(&pID3DDevice));
     
     
-    //创建D3D2命令队列接口
+    //创建DX命令队列
     D3D12_COMMAND_QUEUE_DESC queueDesc = {};
     queueDesc.Type = D3D12_COMMAND_LIST_TYPE_DIRECT;
     pID3DDevice->CreateCommandQueue(&queueDesc, IID_PPV_ARGS(&pICommandQueue));
     
 
     //创建交换链
-    DXGI_SWAP_CHAIN_DESC1 swapChainDesc = {};
+    DXGI_SWAP_CHAIN_DESC1 swapChainDesc = {};//声明交换链结构体变量
+    //填写交换链结构体
     swapChainDesc.BufferCount = nFrameBackBufCount;
     swapChainDesc.Width = iWidth;
     swapChainDesc.Height = iHeight;
@@ -165,19 +184,21 @@ int APIENTRY wWinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance
     swapChainDesc.BufferUsage = DXGI_USAGE_RENDER_TARGET_OUTPUT;
     swapChainDesc.SwapEffect = DXGI_SWAP_EFFECT_FLIP_DISCARD;
     swapChainDesc.SampleDesc.Count = 1;
-
+    //为窗口句柄创建交换链
     pIDXGIFactory5->CreateSwapChainForHwnd(
         pICommandQueue.Get(),
         g_hwnd,
         &swapChainDesc,
         nullptr,
         nullptr,
-        &pISwapChain1
+        &pISwapChain1//此处只能使用低版本交换链
     );
     
     //得到当前后缓冲区的序号，也就是下一个将要呈送显示的缓冲区的序号
+    //As函数是为了使用低版本的SwapChain1接口得到高版本的SwapChain3接口。
+    //目的是为了调用GetCurrentBackBufferIndex方法
     pISwapChain1.As(&pISwapChain3);
-    //注意此处使用了高版本的SwapChain接口的函数
+    //注意此处使用了高版本的SwapChain3接口的函数
     nFrameIndex = pISwapChain3->GetCurrentBackBufferIndex();
 
 
@@ -188,16 +209,17 @@ int APIENTRY wWinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance
     rtvHeapDesc.Flags = D3D12_DESCRIPTOR_HEAP_FLAG_NONE;
 
     pID3DDevice->CreateDescriptorHeap(&rtvHeapDesc, IID_PPV_ARGS(&pIRTVHeap));
-    //得到每个描述符元素的大小
+    //得到每个描述符元素的大小，方便下面遍历时偏移迭代器
     nRTVDescriptorSize = pID3DDevice->GetDescriptorHandleIncrementSize(D3D12_DESCRIPTOR_HEAP_TYPE_RTV);
     
     //创建RTV描述符
     CD3DX12_CPU_DESCRIPTOR_HANDLE rtvHandle(pIRTVHeap->GetCPUDescriptorHandleForHeapStart());
+    //上面rtvHandle可以理解为迭代器，方便为下面for循环创建RTV
     for (UINT i = 0; i < nFrameBackBufCount; i++)
     {
         pISwapChain3->GetBuffer(i, IID_PPV_ARGS(&pIARenderTargets[i]));
         pID3DDevice->CreateRenderTargetView(pIARenderTargets[i].Get(), nullptr, rtvHandle);
-        rtvHandle.Offset(1, nRTVDescriptorSize);
+        rtvHandle.Offset(1, nRTVDescriptorSize);//迭代器offset
     }
 
 
